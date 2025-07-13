b89b5a65f3fd3a1dd307c454684341796cf61c43
Ajay Raj <ajayrajsaini333@gmail.com>
2025-07-13 22:29:55 +0530

    initialized project

---
+++ backend/src/main/java/com/bidsphere/BidsphereApplication.java
@@ -0,0 +1,17 @@
+package com.bidsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidsphereApplication.class, args);
+
+		while(true){
+			System.out.println("Working");
+		}
+	}
+
+}
---
+++ backend/src/test/java/com/bidsphere/BidsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
