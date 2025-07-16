5ffd82b74f001eaf48dd0f12a7078434ff74120e
Ajay Raj <ajayrajsaini333@gmail.com>
2025-07-16 21:55:38 +0530

    partially added login api

---
+++ backend/src/main/java/com/bidsphere/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.bidsphere.service;
+
+import com.bidsphere.model.User;
+import com.bidsphere.repository.UserRepository;
+import com.bidsphere.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username: " + username));
+        return new CustomUserDetails(user);
+    }
+}
